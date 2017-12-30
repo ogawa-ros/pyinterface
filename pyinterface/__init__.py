1c4fc15d1b5a3350b5d1c8888660070dce6150bf
Atsushi Nishimura <ars096@gmail.com>
2017-12-31 00:26:45 +0900

    release v0.4.0

--- pyinterface/__init__.py
+++ pyinterface/__init__.py
@@ -1,6 +1,6 @@
 
 
-__version__ = '0.3.5'
+__version__ = '0.4.0'
 
 
 from .tools import open
@@ -9,6 +9,9 @@ from .core import interface_driver
 
 from . import pci2702
 from . import pci2724
+from . import pci3177
+from . import pci340516
+from . import pci340816
 from . import pci6204
 from . import pci7204
 
