bf0e5412f75491a8fa0bb735397f592fa28a622f
Atsushi Nishimura <ars096@gmail.com>
2018-01-10 13:59:17 +0900

    apply to pass test

--- pyinterface/__init__.py
+++ pyinterface/__init__.py
@@ -3,16 +3,17 @@
 __version__ = '0.4.0'
 
 
-from .tools import open
-
-from .core import interface_driver
-
-from . import pci2702
-from . import pci2724
-from . import pci3177
-from . import pci340516
-from . import pci340816
-from . import pci6204
-from . import pci7204
-
+try:
+    from .tools import open
+    
+    from .core import interface_driver
+    
+    from . import pci2702
+    from . import pci2724
+    from . import pci3177
+    from . import pci340516
+    from . import pci340816
+    from . import pci6204
+    from . import pci7204
+    
 
